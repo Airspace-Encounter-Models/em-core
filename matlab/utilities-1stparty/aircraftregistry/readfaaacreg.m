a6b0de8259b216acd8e1fcb964e1bc7b7257e7db
Andrew Weinert <38636942+aweinert-MIT@users.noreply.github.com>
2021-04-05 10:37:29 -0400

    Update readfaaacreg.m
    Bug fix with inputParser

--- matlab/utilities-1stparty/aircraftregistry/readfaaacreg.m
+++ matlab/utilities-1stparty/aircraftregistry/readfaaacreg.m
@@ -12,7 +12,7 @@ addOptional(p,'inDir',[getenv('AEM_DIR_CORE') filesep 'data' filesep 'FAA-Aircra
 addOptional(p,'outFile',[getenv('AEM_DIR_CORE') filesep 'output' filesep 'acregfaa-' date '.mat']);
 
 % Parse
-p.parse(p,varargin{:});
+parse(p,varargin{:});
 
 %% Load aircraft reference file
 fid = fopen([p.Results.inDir filesep 'ACFTREF.txt'],'r'); % Open File
@@ -83,7 +83,7 @@ engType = strings(numLines,1);
 
 % Not parsed: Assign: Registration
 % regName = string(textMaster{7}); % Registrant Name
-% regState = string(textMaster{11}); % Registrant’s State
+% regState = string(textMaster{11}); % Registrantâ€™s State
 % codeStatus = string(textMaster{21}); % Status Code
 
 % Not parsed: Kit
@@ -93,7 +93,7 @@ engType = strings(numLines,1);
 % Not parsed: Other names
 % textMaster(8); % Street1
 % textMaster(9); % Street2
-% textMaster(10); % Registrant’s City
+% textMaster(10); % Registrantâ€™s City
 % textMaster(12); % Zip Code
 % textMaster(13); % Region
 % textMaster(14); % County Mail
